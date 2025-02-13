18cb8aa2b9c2cfd1a4577cbe05b685e37a64557b
Chinedu <chinedu@Chinedus-MacBook-Pro.local>
2025-02-13 12:27:19 +0100

    updated code

--- app.js
+++ app.js
@@ -1,71 +1,20 @@
 const express = require("express");
 // const morgan = require("morgan");
 // const { logger, authorize } = require("./middleware");
-
-let { people } = require("./data");
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 
 //middleware are functions that execute between the request and response object,
 const app = express();
-
-// static assets;
 
 app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-app.post("/api/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "Please provide name value" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`welcome : ${name}`);
-  } else {
-    res.status(401).send("Please enter a name");
-  }
-});
+app.use("/api/people", people);
+app.use("/login", auth);
+// static assets;
 
-app.put("/api/people/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === +id);
-
-  if (!person) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === +id) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, person: newPeople });
-});
-
-
-app.delete('/api/people/:id', (req, res)=>{
-  const { id } = req.params;
-  const person = people.find((person) => person.id === +id);
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` });
-  }
-  const newPeople = people.filter((person)=> person.id !== +id);
-  return res.status(200).json({ success: true, data: newPeople})
-})
 app.listen(4999, () => {
   console.log("Server is running on port 4999");
 });
